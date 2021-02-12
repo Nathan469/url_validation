86c4e35b4c07f010a558cb7a60e49bd6e4cc6f6a
Nathan469 <porrasalex@hotmail.com>
2021-02-12 19:51:49 +0100

    return response json stream

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 import re
+import requests
+import json
 
 def read_input():
     """Reads a series of strings separated by \n and returns a list.
@@ -12,4 +14,30 @@ def read_input():
     return output
 
 def is_valid_url(url: str) -> bool:
-    return re.match(r"http(s?):\/\/[A-Za-z0-9\-\.\_\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\%\=]+", url) is not None        
+    return re.match(r"http(s?):\/\/[A-Za-z0-9\-\.\_\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\%\=]+", url) is not None 
+
+def main():
+    urls = read_input()
+    for url in urls:
+        if is_valid_url(url):
+            try:
+                response = requests.get(url, timeout=10)
+                print(json.dumps({
+                    "Status_code": response.status_code,
+                    "Url": url,
+                    "Content_length": response.headers.get("Content-Length", "unknown"),
+                    "Date": response.headers.get("Date", "unknown")
+                }, indent = 4))
+            except requests.exceptions.RequestException:
+                print(json.dumps({
+                    "Url": url,
+                    "Error": "RequestException"
+                }, indent = 4), file=sys.stderr)
+        else:
+            print(json.dumps({
+                "Url": url,
+                "Error": "invalid url"
+            }, indent = 4), file=sys.stderr)               
+
+if __name__ == '__main__':
+    main()
--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 import io
+import json
 import main
 import pytest
+import requests
 
 def test_read_input_ok(monkeypatch):
     monkeypatch.setattr('sys.stdin', io.StringIO('a\nb\nc\nd\n'))
@@ -16,4 +18,55 @@ def test_is_valid_url_parameters_returns_true():
     assert main.is_valid_url("https://www.example.com/?bedroom=birthday&bit=airplane")    
 
 def test_is_valid_url_anchors_returns_true():
-    assert main.is_valid_url("http://www.example.com/basket/birthday#border")           
+    assert main.is_valid_url("http://www.example.com/basket/birthday#border")  
+
+def test_main_happycase_ok(monkeypatch, capsys):
+
+    def mock_requests(url, timeout):
+        if url == 'https://golang.org/':
+            response = requests.models.Response()
+            response.status_code = 200
+            response.headers = {
+                    'Date': 'Fri, 12 Feb 2021 18:11:35 GMT',
+                    'Content-Type': 'text/html; charset=utf-8',
+                    'Vary': 'Accept-Encoding',
+                    "Content-Length": "61",
+                    'Transfer-Encoding': 'chunked'}
+            return response
+        
+    monkeypatch.setattr('sys.stdin', io.StringIO('https://golang.org/\n'))
+    monkeypatch.setattr('requests.get', mock_requests)
+    main.main()
+    captured = capsys.readouterr()
+    assert json.loads(captured.out) == json.loads(
+"""{
+    "Status_code": 200,
+    "Url": "https://golang.org/",
+    "Content_length": "61",
+    "Date": "Fri, 12 Feb 2021 18:11:35 GMT"}""")
+
+
+def test_main_invalid_address_outputs_to_stderr(monkeypatch, capsys):
+    monkeypatch.setattr('sys.stdin', io.StringIO('bad://address/\n'))
+    main.main()
+    captured = capsys.readouterr()
+    assert json.loads(captured.err) == json.loads(
+"""{
+    "Url": "bad://address/",
+    "Error": "invalid url"}""")
+
+
+def test_main_timeout_prints_to_stderr(monkeypatch, capsys):
+
+    def mock_requests(url, timeout):
+        if url == 'https://golang.org/':
+            raise requests.exceptions.Timeout()
+        
+    monkeypatch.setattr('sys.stdin', io.StringIO('https://golang.org/\n'))
+    monkeypatch.setattr('requests.get', mock_requests)
+    main.main()
+    captured = capsys.readouterr()
+    assert json.loads(captured.err) == json.loads(
+"""{
+    "Url": "https://golang.org/",
+    "Error": "RequestException"}""")
